2e16cd2afe7057b2e7d7a84989adb3aa097e5925
machilas <marcello.machilas@gmail.com>
2022-04-01 16:31:43 -0300

    Acrecentando axios na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api';
+
+import './App.css';
 
 import Hearder from './components/Header';
 
@@ -10,7 +13,15 @@ import Hearder from './components/Header';
  */
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects')
+            .then(response => {
+                console.log(response)
+                setProjects(response.data)
+            })
+    }, []);
 
     // useState retorna um array com 2 posições
     //
@@ -18,17 +29,28 @@ function App() {
     // 2.  Função para atualizarmos esse valor
 
 
-    function handleAddproject() {
+    async function handleAddproject() {
         // projects.push(`Novo Projeto ${Date.now()}`);
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+        // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+        const response = await api.post('projects',
+            {
+                title: `Novo Projeto ${Date.now()}`,
+                owner: "Marcello Máchilas"
+            });
+
+        const project = response.data
+
+        setProjects([...projects, project])
     }
 
     return (
         <>
             <Hearder title="Projects" />
 
+
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type='button' onClick={handleAddproject}>Adicionar Projeto</button>
